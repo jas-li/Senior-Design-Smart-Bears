3317cc2ab4588cb6bb427d98b1bba46c84658060
howeyour <jli3469@bu.edu>
2024-11-04 15:25:36 -0500

    base script for cv

---
+++ objectdetection.py
@@ -0,0 +1,71 @@
+# Detect objects from images
+# --------------------------
+# import cv2
+# import matplotlib.pyplot as plt 
+# import cvlib as cv
+# from cvlib.object_detection import draw_bbox
+# from cvlib.object_detection import YOLO
+
+# image = cv2.imread("./img/street.jpg") 
+# plt.imshow(image)
+
+# weights = 'yolov3.weights'
+# config = 'yolov3.cfg'
+# labels = 'coco.names'
+# yolo = YOLO(weights, config, labels)
+
+# box, label, count = yolo.detect_objects(image)
+# output = draw_bbox(image, box, label, count)
+
+# # display output
+# cv2.imshow("object detection", output)
+# cv2.waitKey()
+
+# Detect objects through webcam
+# -----------------------------
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
+import cv2
+from cvlib.object_detection import YOLO
+
+weights = 'yolov3.weights'
+config = 'yolov3.cfg'
+labels = 'coco.names'
+yolo = YOLO(weights, config, labels)
+
+# open webcam
+webcam = cv2.VideoCapture(0)
+
+if not webcam.isOpened():
+    print("Could not open webcam")
+    exit()
+    
+
+# loop through frames
+while webcam.isOpened():
+
+    # read frame from webcam 
+    status, frame = webcam.read()
+
+    if not status:
+        print("Could not read frame")
+        exit()
+
+    # apply object detection
+    bbox, label, conf = yolo.detect_objects(frame)
+
+    print(bbox, label, conf)
+
+    # draw bounding box over detected objects
+    out = draw_bbox(frame, bbox, label, conf)
+
+    # display output
+    cv2.imshow("Real-time object detection", out)
+
+    # press "Q" to stop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+# release resources
+webcam.release()
+cv2.destroyAllWindows()        
