d4267b1c85abb979927de5d3bf197b4307766791
howeyour <jli3469@bu.edu>
2024-11-21 03:45:57 -0500

    update wifi

--- tinypico/wifi.py
+++ tinypico/wifi.py
@@ -4,28 +4,31 @@ import socket
 import math
 from machine import Pin, PWM
 
-
-def notification(pin):
+def pulse(pin):
     for _ in range(3):
-        pin.on()   # Alternatively: pin.value(1)
-        time.sleep(.25)  # Keep it high for 1 second
-        pin.off()  # Alternatively: pin.value(0)
-        time.sleep(.1)  # Keep it low for 1 second
+        pin.on()   
+        time.sleep(0.25) 
+        pin.off()  
+        time.sleep(0.25)  
         
-def loading(pin):
-    pwm = PWM(pin)
+def increment(pin):
+    pwm = PWM(Pin(pin))
     pwm.freq(50)
-    for _ in range(2):
-        for x in range(50):
-            duty_cycle = 256 + math.floor(256 * (1 + (math.sin(2 * math.pi * x / 50))))
-            pwm.duty(duty_cycle)
-            time.sleep(0.05)
-    pwm.duty(0)
+
+    for x in range(0, 564, 20):  # Increment from 0 to 1023 in steps of 20
+        pwm.duty(x) 
+        time.sleep(0.25)  
+
+    pwm.duty(0)  # Stop the PWM after reaching maximum intensity
     
+def constant(pin):
+    pin.on()   
+    time.sleep(2)
+    pin.off()    
     
-pin = Pin(14, Pin.OUT)
-pin.on()
-time.sleep(10)
+analog_pin = Pin(14, Pin.OUT)
+digital_pin = Pin(15, Pin.OUT)
+
 ssid = 'smartbears'
 password = ''
 
@@ -41,7 +44,7 @@ print("Connected to Wi-Fi!")
 print("IP address:", station.ifconfig()[0])
 
 # Define server IP and port
-SERVER_IP = '192.168.1.101'  # Replace with your server's IP address
+SERVER_IP = '192.168.1.125'  # Replace with your server's IP address
 SERVER_PORT = 12345
 
 # Create a UDP socket
@@ -49,21 +52,27 @@ udp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 message = "Hello, UDP Server!"
 
-# Send the message to the server
-udp_client_socket.sendto(message.encode(), (SERVER_IP, SERVER_PORT))
+while True:
+    # Send the message to the server
+    udp_client_socket.sendto(message.encode(), (SERVER_IP, SERVER_PORT))
 
-# Receive response from the server
-response, server_address = udp_client_socket.recvfrom(1024)
-print(f"Received from server: {response.decode()}")
+    # Receive response from the server
+    response, server_address = udp_client_socket.recvfrom(1024)
+    print(f"Received from server: {response.decode()}")
 
+    value = response.decode()
+
+    if value == '1':
+        pulse(digital_pin)
+    elif value == '2':
+        increment(analog_pin)
+    elif value == '3':
+        constant(digital_pin)
+        
+    time.sleep(4)
+        
 # Close the socket
 udp_client_socket.close()
 
-value = response.decode()
-
-if value == '1':
-    loading(pin)
-elif value == '2':
-    notification(pin)
 
 
